bfb94e631d6663b40a804c9f4554174b2fb17ecf
Saad Qasim <m.saadqasim7665@gmail.com>
2025-07-06 01:19:51 +0500

    Implemented Candidate and Voter classes with eligibility methods

--- index.java
+++ index.java
@@ -28,6 +28,39 @@ class PollingStation {
         this.location = location;
     }
 }
+class Candidate extends Person {
+    boolean hasDualNationality, isAhmadi, hasDeclaredAssets;
+    PoliticalParty party;
+
+    Candidate(String name, int age, String nationality, boolean dual, boolean ahmadi, boolean assets, PoliticalParty party) {
+        super(name, age, nationality);
+        hasDualNationality = dual;
+        isAhmadi = ahmadi;
+        hasDeclaredAssets = assets;
+        this.party = party;
+    }
+
+    @Override
+    boolean isEligible() {
+        return age >= 25 && age <= 40 && nationality.equalsIgnoreCase("pakistani") && !isAhmadi && hasDeclaredAssets;
+    }
+}
+
+class Voter extends Person {
+    boolean isMentallyStable;
+    PollingStation station;
+
+    Voter(String name, int age, String nationality, boolean stable, PollingStation station) {
+        super(name, age, nationality);
+        isMentallyStable = stable;
+        this.station = station;
+    }
+
+    @Override
+    boolean isEligible() {
+        return age >= 18 && age <= 60 && isMentallyStable;
+    }
+}
 
 
 
